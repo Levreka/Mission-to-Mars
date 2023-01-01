c34f451366ee1699cb3d8ae0366e462d1b1f8fdc
Franklin Tello <franklintello@msn.com>
2023-01-01 16:27:57 -0700

    updating app.py code and bugs

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#flask run needs to be run using command promp in visual studio go to by were the output is seen and on the plus sign add commmand prompt terminal 
+#and type flask run in there
 
 
 # Import Flask, PyMongo, and scraping.py 
@@ -18,22 +20,27 @@ app= Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-# Set Up Routes
+# app.config["MONGO_URI"] tells Python that our app will connect to Mongo using a URI, a uniform resource identifier similar to a URL.
+# "mongodb://localhost:27017/mars_app" is the URI we'll be using to connect our app to Mongo. This URI is saying that the app can reach Mongo through our localhost server, using port 27017, using a database named "mars_app".
+
+# Set Up Route for the html page
 @app.route("/")
 def index():
     mars=mongo.db.mars.find_one()
     return render_template('index.html', mars=mars)
 
+#set up scraping route view 1051 module for more explanation on this code
 @app.route("/scrape")
 def scrape():
     mars=mongo.db.mars
     #holds newly scraped data, referencing scrape_all() function in scraping.py file
     mars_data=scraping.scrape_all()
-    mars.update({},mars_data,upsert=True)
-    return "Scraping Successful"
+    #update the database using .update_one()
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
+    return redirect('/', code=302)
 
+#run the flask app remember this is done in the terminal
 if __name__ == "__main__":
     app.run(debug=True)
 
 
-#
