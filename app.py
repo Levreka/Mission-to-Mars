99fbedd5e0c6c16351daa1d91429df1ea4af75a7
Franklin Tello <franklintello@msn.com>
2023-01-01 15:54:27 -0700

    updating comments and code

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scraping
 
+# Let's break down what this code is doing.
+
+# The first line says that we'll use Flask to render a template, redirecting to another url, and creating a URL.
+# The second line says we'll use PyMongo to interact with our Mongo database.
+# The third line says that to use the scraping code, we will convert from Jupyter notebook to Python.
+
+#set up our flask app
 app= Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config['MONGO_URI']= "mongodb://localhost:27017/mars_app"
-mongo=PyMongo(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 # Set Up Routes
 @app.route("/")
